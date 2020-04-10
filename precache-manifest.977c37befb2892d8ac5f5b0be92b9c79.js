f2af4ec8e8298fe3c73c953b03611181e87e8677
Ramesh Kumar Ramu <rameshk2k2@gmail.com>
2020-04-10 16:26:49 -0400

    Updates

---
+++ precache-manifest.977c37befb2892d8ac5f5b0be92b9c79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20df936bc1e8ae7792d7c0fdfe3f7a37",
+    "url": "/fuze-test/index.html"
+  },
+  {
+    "revision": "54f9928e4391ddd8a414",
+    "url": "/fuze-test/static/css/2.9f2893ca.chunk.css"
+  },
+  {
+    "revision": "8bf0b7cb5bf37758203b",
+    "url": "/fuze-test/static/css/main.035230d7.chunk.css"
+  },
+  {
+    "revision": "54f9928e4391ddd8a414",
+    "url": "/fuze-test/static/js/2.ae0d563c.chunk.js"
+  },
+  {
+    "revision": "6b1fbaab5507e10506120199166aab93",
+    "url": "/fuze-test/static/js/2.ae0d563c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bf0b7cb5bf37758203b",
+    "url": "/fuze-test/static/js/main.896c5065.chunk.js"
+  },
+  {
+    "revision": "02d4a46e9ac7f41ade0a",
+    "url": "/fuze-test/static/js/runtime-main.bb942eda.js"
+  }
+]);
