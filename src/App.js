5219321ea65b8d48fff86e8cb4df02c404ae2d92
Ramesh Kumar Ramu <rameshk2k2@gmail.com>
2020-04-11 00:17:36 -0400

    Added Reach Router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Route,
-} from 'react-router-dom';
+import { Router } from '@reach/router';
 import HomePage from './pages/HomePage.js';
-// import About from './pages/About.js';
-// import Nav from './components/Nav.js';
 import Files from './pages/Files.js';
 import ApiTest from './pages/ApiTest.js';
 
 function App() {
 
   return (
-    <Router>
-      <div className="App">
-        <Route path='/' component={HomePage} exact />
-        {/* <Route path='/about' component={About} /> */}
-        <Route path='/files' component={Files} />
-        <Route path='/api-test' component={ApiTest} />
-      </div>
-    </Router>
+    <div className="App">
+      <Router>
+        <HomePage path='/' />
+        <Files path='/files' />
+        <ApiTest path='/api-test' />
+      </Router>
+    </div>
   );
 }
 
